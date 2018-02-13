f942172b5c165c7a706e1a1580e4b68274f27253
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-02-13 15:29:35 +0300

    add xml for basketball project

---
+++ app/src/main/java/com/example/android/basketball/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.android.basketball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int score;
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(1);
+    }
+    public void scoreThree(View view){
+        score=score+3;
+        display(score);
+    }
+    public void scoreTwo(View view){
+        score=score+2;
+        display(score);
+    }
+    public void scoreFreeThrow(View view){
+        score=score+1;
+        display(score);
+    }
+    public void display (int number){
+        TextView countViewer = (TextView)findViewById(R.id.team_a_score);
+        countViewer.setText(""+ number);
+    }
+}
